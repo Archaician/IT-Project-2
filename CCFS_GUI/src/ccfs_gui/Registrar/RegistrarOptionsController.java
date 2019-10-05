f9364a06493c62331751badd7ab22b0252147c48
Imran-D-Mahmood <48464030+Imran-D-Mahmood@users.noreply.github.com>
2019-10-05 13:25:16 +0800

    Add files via upload

--- CCFS_GUI/src/ccfs_gui/Registrar/RegistrarOptionsController.java
+++ CCFS_GUI/src/ccfs_gui/Registrar/RegistrarOptionsController.java
@@ -8,8 +8,6 @@ package ccfs_gui.Registrar;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -23,7 +21,7 @@ import javafx.scene.layout.GridPane;
 /**
  * FXML Controller class
  *
- * @author Acer
+ * @author Imran
  */
 public class RegistrarOptionsController implements Initializable {
 
@@ -40,50 +38,55 @@ public class RegistrarOptionsController implements Initializable {
         @FXML
         private GridPane inner_borderpane;
         @FXML
-        private Button main_options_buttons1;
+        private Button registerStud_Btn;
         @FXML
-        private Button main_options_buttons2;
+        private Button enrollContStud_Btn;
         @FXML
-        private Button main_options_buttons3;
+        private Button viewStudList_Btn;
         @FXML
-        private Button main_options_buttons4;
+        private Button viewArchive_Btn;
         @FXML
-        private Button main_options_buttons5;
+        private Button attendance_Btn;
         @FXML
-        private Button main_options_buttons6;
+        private Button studGrades_Btn;
         @FXML
-        private Button main_options_buttons7;
+        private Button studDiscount_Btn;
         @FXML
-        private Button main_options_buttons8;
+        private Button studSponsorship_Btn;
         @FXML
-        private Button main_options_buttons9;
+        private Button studChecklist_Btn;
         @FXML
-        private Button main_options_buttons10;
+        private Button generateReports_Btn;
         @FXML
         private AnchorPane right_outer_pane;
         @FXML
         private AnchorPane innerpane;
+        
+        
+        private void anchorPaneConstraints(BorderPane root) {
+                AnchorPane.setTopAnchor(root, 0.0);
+                AnchorPane.setRightAnchor(root, 0.0);
+                AnchorPane.setLeftAnchor(root, 0.0);
+                AnchorPane.setBottomAnchor(root, 0.0);
+        }
+        
+        @FXML
+        private void registrarOptionsButtons(ActionEvent event) throws IOException {
+                BorderPane root = null;
+                
+                if (event.getSource() == registerStud_Btn) {
+                        root = FXMLLoader.load(getClass().getResource("/ccfs_gui/Enrollment/EnrollmentNew1FXML.fxml"));
+                } else if (event.getSource() == enrollContStud_Btn) {
+                        root = FXMLLoader.load(getClass().getResource("/ccfs_gui/Enrollment/EnrollmentContinuingFXML.fxml"));
+                }
+                
+                innerpane.getChildren().setAll(root);
+                anchorPaneConstraints(root);
+        }
+        
         /**
          * Initializes the controller class.
          */
-        private void anchorPaneConstraints(BorderPane root) {
-            AnchorPane.setTopAnchor(root, 0.0);
-            AnchorPane.setRightAnchor(root, 0.0);
-            AnchorPane.setLeftAnchor(root, 0.0);
-            AnchorPane.setBottomAnchor(root, 0.0);
-    }
-        
-        @FXML
-        public void viewGrades(ActionEvent event) {
-                try {
-                        BorderPane root = FXMLLoader.load(getClass().getResource("ViewGrades.fxml"));
-                        innerpane.getChildren().setAll(root);
-                        anchorPaneConstraints(root);
-                } catch (IOException ex) {
-                        Logger.getLogger(RegistrarOptionsController.class.getName()).log(Level.SEVERE, null, ex);
-                }
-        }
-        
         @Override
         public void initialize(URL url, ResourceBundle rb) {
                 // TODO
