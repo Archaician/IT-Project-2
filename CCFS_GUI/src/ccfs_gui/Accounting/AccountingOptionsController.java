633d0c1173adfd86668fbc12ae9eb82e4d59da33
Imran-D-Mahmood <48464030+Imran-D-Mahmood@users.noreply.github.com>
2019-10-10 11:19:22 +0800

    Separate main stage from options.

---
+++ CCFS_GUI/src/ccfs_gui/Accounting/AccountingOptionsController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ccfs_gui.Accounting;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Acer
+ */
+public class AccountingOptionsController implements Initializable {
+
+        @FXML
+        private AnchorPane container;
+        @FXML
+        private ButtonBar bottom_inner_pane;
+        @FXML
+        private Button logout_button;
+
+        /**
+         * Initializes the controller class.
+         */
+        @Override
+        public void initialize(URL url, ResourceBundle rb) {
+                // TODO
+        }        
+        
+}
