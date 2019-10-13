fa5a63d2d2609225fb65982613c2aa4e23ca2d85
Imran-D-Mahmood <48464030+Imran-D-Mahmood@users.noreply.github.com>
2019-10-13 12:49:20 +0800

    Add home button.

--- CCFS_GUI/src/ccfs_gui/Admin/AdminMainStageController.java
+++ CCFS_GUI/src/ccfs_gui/Admin/AdminMainStageController.java
@@ -41,6 +41,11 @@ public class AdminMainStageController implements Initializable {
                 LayoutProperties.anchorPaneConstraints(root);
         }
         
+        @FXML
+        private void homeButton() throws IOException {
+                initialContent();
+        }
+        
         
         /**
          * Initializes the controller class.
