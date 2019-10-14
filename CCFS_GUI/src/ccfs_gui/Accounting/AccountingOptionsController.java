37c16676f1567eb7a58a2ee4d4f993e3d26b0b64
Paul Perez <45648270+Archaician@users.noreply.github.com>
2019-10-14 15:36:51 +0800

    Added Button Linking Functionality
    Yes

--- CCFS_GUI/src/ccfs_gui/Accounting/AccountingOptionsController.java
+++ CCFS_GUI/src/ccfs_gui/Accounting/AccountingOptionsController.java
@@ -5,9 +5,13 @@
  */
 package ccfs_gui.Accounting;
 
+import ccfs_gui.LayoutProperties;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
@@ -26,7 +30,23 @@ public class AccountingOptionsController implements Initializable {
     private ButtonBar bottom_inner_pane;
     @FXML
     private Button logout_button;
+    @FXML
+    private Button viewAddPayments_Btn;
+    @FXML
+    private Button generateAccReports_Btn;
 
+    @FXML
+     private void accountingOptionsButtons(ActionEvent event) throws IOException {
+         AnchorPane root = null;
+         
+          if (event.getSource() == viewAddPayments_Btn) {
+              root = FXMLLoader.load(getClass().getResource("/ccfs_gui/Accounting/ViewAddPayments.fxml"));
+         } else if (event.getSource() == generateAccReports_Btn) {
+              root = FXMLLoader.load(getClass().getResource("/ccfs_gui/Accounting/AccountingReports.fxml"));
+         }
+         container.getChildren().setAll(root);
+         LayoutProperties.anchorPaneConstraints(root);
+     }
     /**
      * Initializes the controller class.
      */
---
+++ CCFS_GUI/src/ccfs_gui/Accounting/ViewAddPaymentsController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ccfs_gui.Accounting;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Lenovo
+ */
+public class ViewAddPaymentsController implements Initializable {
+
+    @FXML
+    private BorderPane innerpane;
+    @FXML
+    private AnchorPane container;
+
+    private void anchorPaneConstraints(BorderPane root) {
+            AnchorPane.setTopAnchor(root, 0.0);
+            AnchorPane.setRightAnchor(root, 0.0);
+            AnchorPane.setLeftAnchor(root, 0.0);
+            AnchorPane.setBottomAnchor(root, 0.0);
+        }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
