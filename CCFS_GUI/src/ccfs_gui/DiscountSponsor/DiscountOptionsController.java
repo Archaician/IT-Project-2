a996fcb35a6e6377c409f0f1bba6c4791aa1f3da
Paul Perez <45648270+Archaician@users.noreply.github.com>
2019-11-03 04:35:17 +0800

    Updated with label

--- CCFS_GUI/src/ccfs_gui/DiscountSponsor/DiscountOptionsController.java
+++ CCFS_GUI/src/ccfs_gui/DiscountSponsor/DiscountOptionsController.java
@@ -14,6 +14,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
@@ -39,6 +40,8 @@ public class DiscountOptionsController implements Initializable {
     private Button confrm_Btn;
     @FXML
     private TextField searchbar;
+    @FXML
+    private Label nameLabel;
 
     private void selectDiscountPercentage() {
         list.removeAll(list);
