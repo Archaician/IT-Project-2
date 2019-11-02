626f7fa6e50755e602f16a1ef3c91779fa477d8b
Imran-D-Mahmood <48464030+Imran-D-Mahmood@users.noreply.github.com>
2019-11-02 12:46:32 +0800

    Add files via upload

---
+++ CCFS_GUI/src/ccfs_gui/Admin/ListOfSchoolYearsController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ccfs_gui.Admin;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Imran
+ */
+public class ListOfSchoolYearsController implements Initializable {
+
+    @FXML
+    private AnchorPane container;
+    @FXML
+    private BorderPane innerpane;
+    @FXML
+    private Button back_Btn;
+
+    @FXML
+    private void backButton(ActionEvent event) {
+        //TODO
+    }
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
