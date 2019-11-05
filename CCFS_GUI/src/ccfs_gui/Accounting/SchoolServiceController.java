1b08a0a3cb6de2140e66516cda74181328fe495c
Imran-D-Mahmood <48464030+Imran-D-Mahmood@users.noreply.github.com>
2019-11-05 21:25:35 +0800

    Add files via upload

--- CCFS_GUI/src/ccfs_gui/Accounting/SchoolServiceController.java
+++ CCFS_GUI/src/ccfs_gui/Accounting/SchoolServiceController.java
@@ -14,6 +14,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -70,8 +71,7 @@ public class SchoolServiceController implements Initializable {
             if (alert.getResult() == ButtonType.YES) {
                 //TODO
                 DialogWindows.dialogBox(Alert.AlertType.INFORMATION, "Added School Service Fee", "Successfully added school service fee.", ButtonType.OK);
-                //Stage stage = (Stage) ButtonType.OK.getScene().getWindow();
-                //stage.close();
+                ((Node) event.getSource()).getScene().getWindow().hide();
             } else {
                 alert.close();
             }
