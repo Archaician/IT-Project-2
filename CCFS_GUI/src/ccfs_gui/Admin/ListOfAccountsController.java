a125128c6f4d115dd9994237441fd6ee1b46fc02
Imran-D-Mahmood <48464030+Imran-D-Mahmood@users.noreply.github.com>
2019-11-10 12:55:13 +0800

    Populate accounts list table from database.

--- CCFS_GUI/src/ccfs_gui/Admin/ListOfAccountsController.java
+++ CCFS_GUI/src/ccfs_gui/Admin/ListOfAccountsController.java
@@ -8,23 +8,37 @@ package ccfs_gui.Admin;
 import ccfs_gui.LayoutProperties;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.util.Callback;
 
 /**
  * FXML Controller class
  *
- * @author Acer
+ * @author Imran
  */
 public class ListOfAccountsController implements Initializable {
+    
+    private ObservableList<Accounts> acclist;
 
     @FXML
     private AnchorPane container;
@@ -33,17 +47,84 @@ public class ListOfAccountsController implements Initializable {
     @FXML
     private Button back_Btn;
     @FXML
-    private TableView<?> accountslist_Table;
+    private TableView<Accounts> accountslist_Table;
     @FXML
-    private TableColumn<?, ?> name_Col;
+    private TableColumn<Accounts, Integer> accid_Col;
     @FXML
-    private TableColumn<?, ?> username_Col;
+    private TableColumn<Accounts, String> name_Col;
     @FXML
-    private TableColumn<?, ?> accounttype_Col;
+    private TableColumn<Accounts, String> username_Col;
     @FXML
-    private TableColumn<?, ?> accountstatus_Col;
+    private TableColumn<Accounts, String> accounttype_Col;
     @FXML
-    private TableColumn<?, ?> action_Col;
+    private TableColumn<Accounts, String> accountstatus_Col;
+    @FXML
+    private TableColumn action_Col;
+    
+    private static Connection con;
+    
+    private void dbaseConnection() throws SQLException {
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ccfs?"
+                + "user=root&password=");
+    }
+    
+    private void populateAccountsListTable() throws SQLException {
+        acclist = FXCollections.observableArrayList();
+        
+        dbaseConnection();
+        ResultSet rs = con.createStatement().executeQuery("SELECT `accid`, `lname`, `username`, `type`, `accstatus` FROM `accounts`");
+        while (rs.next()) {
+            Accounts accounts = new Accounts();
+            accounts.setAccountID(rs.getInt("accid"));
+            accounts.setName(rs.getString("lname")); //must be combination of firstname and lastname
+            accounts.setUsername(rs.getString("username"));
+            accounts.setAccountType(rs.getString("type"));
+            accounts.setAccountStatus(rs.getString("accstatus"));
+            
+            acclist.add(accounts);
+        }
+        
+        accid_Col.setCellValueFactory(new PropertyValueFactory<>("accountID"));
+        name_Col.setCellValueFactory(new PropertyValueFactory<>("name"));
+        username_Col.setCellValueFactory(new PropertyValueFactory<>("username"));
+        accounttype_Col.setCellValueFactory(new PropertyValueFactory<>("type"));
+        accountstatus_Col.setCellValueFactory(new PropertyValueFactory<>("status"));
+        
+        /*Create a button under Action column for every row.*/
+        Callback<TableColumn<Accounts, String>, TableCell<Accounts, String>> cellFactory = (p) -> {
+            
+            final TableCell<Accounts, String> cell = new TableCell<Accounts, String>() {
+                
+                @Override
+                public void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+                    
+                    if (empty) {
+                        setGraphic(null);
+                        setText(null);
+                    } else {
+                        final Button editBtn = new Button("Edit");
+                        editBtn.setOnAction(e -> {
+                            Accounts acc = getTableView().getItems().get(getIndex());
+                            
+                            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                            alert.show();
+                        });
+                        
+                        setGraphic(editBtn);
+                        //setText(null);
+                        //editBtn.setStyle("-fx-background-color: green");
+                    }
+                }
+            };
+            
+            return cell;
+        };
+        
+        action_Col.setCellFactory(cellFactory);
+        
+        accountslist_Table.setItems(acclist);
+    }
     
     @FXML
     private void backButton(ActionEvent event) throws IOException {
@@ -57,7 +138,12 @@ public class ListOfAccountsController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            dbaseConnection();
+            populateAccountsListTable();
+        } catch (SQLException ex) {
+            Logger.getLogger(ListOfAccountsController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }    
     
 }
