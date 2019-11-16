940b42e870cce226d6b502dd293e4949f37c8ff8
Imran-D-Mahmood <48464030+Imran-D-Mahmood@users.noreply.github.com>
2019-11-16 12:36:47 +0800

    Update UI for creating schoolyear.

--- CCFS_GUI/src/ccfs_gui/Admin/ListOfSchoolYearsController.java
+++ CCFS_GUI/src/ccfs_gui/Admin/ListOfSchoolYearsController.java
@@ -6,7 +6,6 @@
 package ccfs_gui.Admin;
 
 import ccfs_gui.DialogWindows;
-import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -20,10 +19,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -34,8 +30,6 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 import javafx.util.Callback;
 
 /**
