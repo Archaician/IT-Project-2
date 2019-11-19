62805d3d8c58f11860bba7859f8c76e34088b7c9
Imran-D-Mahmood <48464030+Imran-D-Mahmood@users.noreply.github.com>
2019-11-19 23:22:40 +0800

    Remove required field warning for previous school.

--- CCFS_GUI/src/ccfs_gui/Enrollment/RegistrationPageOneController.java
+++ CCFS_GUI/src/ccfs_gui/Enrollment/RegistrationPageOneController.java
@@ -114,8 +114,8 @@ public class RegistrationPageOneController implements Initializable {
         /*Required textfields validation.*/
 //        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-ddd");
         if (surname.getText().isEmpty() || givenname.getText().isEmpty() || middlename.getText().isEmpty()
-                || birthplace.getText().isEmpty() || address.getText().isEmpty() || prevschool.getText().isEmpty() || birthdate.getValue() == null) {
-            FieldValidation.requiredTextFieldWarning(surname, givenname, middlename, birthplace, address, prevschool);
+                || birthplace.getText().isEmpty() || address.getText().isEmpty() || birthdate.getValue() == null) {
+            FieldValidation.requiredTextFieldWarning(surname, givenname, middlename, birthplace, address);
             FieldValidation.requiredDateWarning(birthdate);
         } else {
             if (ClientCon.stub.getLastID().equals("0") == true) {
