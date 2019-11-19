8d781b86723c98b30e46e69c3a5a67570a8a5965
nekoshka <2165086@slu.edu.ph>
2019-11-20 00:51:55 +0800

    started registrars

--- ccfs/ccfs/src/Registrars/Registrars.java
+++ ccfs/ccfs/src/Registrars/Registrars.java
@@ -6,4 +6,13 @@ package Registrars;
  */
 public class Registrars {
     //import the module of admin here enroll 
+    String info[];
+    
+    public Registrars(String[] info) {
+        this.info = info;
+    }
+    
+    public String[] createAc() {
+        return this.info;
+    }
 }
