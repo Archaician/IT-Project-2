5e144c7dd7808a7548517ef839a4882eb8e410fe
Imran-D-Mahmood <48464030+Imran-D-Mahmood@users.noreply.github.com>
2019-11-25 22:51:28 +0800

    Fix mainstage header and show active schyr on homepage.

--- CCFS_GUI/src/ccfs_gui/Accounting/AccountingOptionsController.java
+++ CCFS_GUI/src/ccfs_gui/Accounting/AccountingOptionsController.java
@@ -5,16 +5,23 @@
  */
 package ccfs_gui.Accounting;
 
+import ccfs_gui.Admin.AdminOptionsController;
 import ccfs_gui.LayoutProperties;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -23,7 +30,9 @@ import javafx.scene.layout.AnchorPane;
  * @author Imran
  */
 public class AccountingOptionsController implements Initializable {
-
+    
+    @FXML
+    private Label schyr;
     @FXML
     private AnchorPane container;
     @FXML
@@ -34,6 +43,15 @@ public class AccountingOptionsController implements Initializable {
     private Button viewAddPayments_Btn;
     @FXML
     private Button generateAccReports_Btn;
+    
+    private void setSchoolYear() throws Exception {
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ccfs?"
+                + "user=root&password=");        
+        ResultSet rs = con.createStatement().executeQuery("SELECT CONCAT(yearstart, '-', yearend) FROM `schoolyear` WHERE Status='active'");
+        while (rs.next()) {
+            schyr.setText(rs.getString("CONCAT(yearstart, '-', yearend)"));
+        }
+    }
 
     @FXML
      private void accountingOptionsButtons(ActionEvent event) throws IOException {
@@ -52,6 +70,11 @@ public class AccountingOptionsController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        try {
+            setSchoolYear();
+        } catch (Exception ex) {
+            Logger.getLogger(AdminOptionsController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         // TODO
     }
 
